439fd60b7a0f064921ac95d4549e2cc7dafa8924
John Rigler <john@rigler.org>
2019-06-12 10:10:26 -0400

    Added hard links between repo public_html and Apache version

--- html/1_01.php
+++
@@ -1,8 +0,0 @@
-<p>
-Welcome to Dimecash.  This site is currently undergoing massive construction.  If, however, you don't mind seeing the innards of a new system laid out before you, then head to the browser link to see how the Dimecash initiative is already at work prototyping services live on Dogecoin. If you have a dogecoin wallet, then you can be part of the fun.  
-<p>
-You might also want to check out the official github page and see the Dimecash whitepaper:
-
-<a href=https://github.com/johnrigler/dimecash> Dimecash </a>
-<br>
-
