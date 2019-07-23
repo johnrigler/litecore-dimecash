42dab5179474e812c639f531e08dd06b166e1d45
John Rigler <john@rigler.org>
2019-07-23 17:44:14 +0000

    add recent blocks into the system

--- dc.bash
+++ dc.bash
@@ -8,9 +8,10 @@ alias php=/usr/local/php70/bin/php
 
 . ~/alp/alp.bash
 
+: Change this to point to the dimecash directory
 export _DC_DIR=~/dimecash
-export _DC_LIB=$DC_DIR/php_library
-export _DC_SRC=$DC_DIR/dc.bash
+export _DC_LIB=$_DC_DIR/php_library
+export _DC_SRC=$_DC_DIR/dc.bash
 export _DC_WEB=~/public_html
 
 
